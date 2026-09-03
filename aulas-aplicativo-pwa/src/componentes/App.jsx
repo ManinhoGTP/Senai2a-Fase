b6616bc1d745c4bde8eba62b80895875bdee80b9
GABRIEL TULER PEREIRA <gabriel_pereira183@estudante.sesisenai.org.br>
2026-09-02 21:59:38 -0300

    aulas aplicativo

---
+++ aulas-aplicativo-pwa/src/componentes/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Cabecalho from './cabecalho/Cabecalho'
+import Principal from './principal/Principal'
+import Rodape from './rodape/Rodapé'
+
+
+function App() {
+
+  return (
+    <div>
+      <Cabecalho/>
+      <Principal />
+      <Rodape />
+    </div>
+  )
+}
+
+export default App
