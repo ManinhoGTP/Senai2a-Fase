020de1829abab2c936ee80f0e636530582f9451b
ManinhoGTP <gabrieltuler99@gmail.com>
2026-09-09 21:50:49 -0300

    css

--- 01.revisao/scripts/outros/exercicio-1.js
+++
@@ -1,8 +0,0 @@
-function exercicio1(){
-
-let nome = "gabriel", idade = 17
-
-console.log(`Olá meu nome é ${nome} e eu tenho ${idade} anos`)
-}
-
-export default exercicio1;
--- 01.revisao/scripts/outros/exercicio-7.js
+++
@@ -1,8 +0,0 @@
-function exercicio7(){
-
-for(let i = 1; i <= 100; i++){
-    console.log(i)
-}
-}
-
-export default exercicio7;
