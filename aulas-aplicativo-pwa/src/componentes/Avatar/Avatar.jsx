a58614f53b336a4c3732c0e14da309ce03f9d4bf
ManinhoGTP <gabrieltuler99@gmail.com>
2026-09-17 15:04:05 -0300

    Update Avatar.jsx

--- aulas-aplicativo-pwa/src/componentes/Avatar/Avatar.jsx
+++ aulas-aplicativo-pwa/src/componentes/Avatar/Avatar.jsx
@@ -9,7 +9,7 @@ function Avatar(props){
         avatarTexto = nomes[0].charAt(0) + nomes.at(-1).charAt(0)
         
         
-    }else if(nomes.length = 1){
+    }else if(nomes.length == 1){
         avatarTexto = nomes[0].charAt(0)
     }
     
